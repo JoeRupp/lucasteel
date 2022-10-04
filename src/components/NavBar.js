ab24a3e00739c6a9db48e1ad1ab8d2b66f724029
Joe Rupp <joerpp8@gmail.com>
2022-10-03 20:57:15 -0600

    Add nav bar functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,22 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const NavBar = () => {
-  <Nav>
-    <h1>LucaSteel</h1>
-    <LinkContainer>
-      <NavigationLink to="/" onClick={() => console.log("home")}>
-        Home
-      </NavigationLink>
-      <NavigationLink to="/contact" onClick={() => console.log("contact")}>
-        Contact
-      </NavigationLink>
-      <NavigationLink to="/work" onClick={() => console.log("work")}>
-        Work
-      </NavigationLink>
-    </LinkContainer>
-  </Nav>;
+  return (
+    <Nav>
+      <h1>LucaSteel</h1>
+      <LinkContainer>
+        <NavigationLink to="/" onClick={() => console.log("home")}>
+          Home
+        </NavigationLink>
+        <NavigationLink to="/contact" onClick={() => console.log("contact")}>
+          Contact
+        </NavigationLink>
+        <NavigationLink to="/work" onClick={() => console.log("work")}>
+          Work
+        </NavigationLink>
+      </LinkContainer>
+    </Nav>
+  );
 };
 
 export default NavBar;
