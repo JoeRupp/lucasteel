e317137e06c27ae6aae0d37d078b4b6c4007b241
Joe Rupp <joerpp8@gmail.com>
2022-12-07 17:16:44 -0700

    Add styled team member profiles to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -14,31 +14,38 @@ const AboutPage = () => {
           Colorado. We started the shop with the goal to create one-of-a-kind
           products that fit the need of all and any clients. We have the skills
           and experience to create any project of the highest quality possible.
+          <br />
+          <br />
           We pride ourselves on the ability to think out of the box and
           problem-solve to create solutions that are practical and look
-          excellent. Get to know us better and check out the team below!
+          excellent.
+          <br />
+          <br />
+          Get to know us better and check out the team below!
         </AboutText>
       </GeneralAboutSection>
       <TeamSection>
-        <SubHeader>Meet the Team:</SubHeader>
-        <TeamMemberProfile
-          name="Lucas Onorato"
-          role="Owner/Founder"
-          bio="If you can dream it we can build it.” With five years of experience welding and fabricating, Lucas has turned his hobby into a business where anything can be made. With a background of working with talented local artists, Lucas quickly excelled in welding sculptures and decorative furnishings. With a passion for creativity and an eye for detail, Lucas can find the solutions clients need while fitting a unique artistic design."
-          picture="LucasProfile.png"
-        />
-        <TeamMemberProfile
-          name="Zachary Miner"
-          role="Shop Manager/CAD Engineer"
-          bio="Graduating from Colorado School of Mines with a degree in Mechanical Engineering, Zachary uses his engineering experience and forward-thinking design to create the 3D CAD design. He then uses his welding skills to bring these ideas to life. "
-          picture="ZachProfile.png"
-        />
-        <TeamMemberProfile
-          name="Hanna Rupp"
-          role="Business Manager"
-          bio="With a degree in Finance from the University of Denver and a background in Financial Planning and Analysis, Hanna oversees that the business runs smoothly. From bookkeeping to research, she keeps the numbers straight and the office in order."
-          picture="HannaProfile.png"
-        />
+        <SubHeaderTwo>Meet the Team:</SubHeaderTwo>
+        <TeamProfileOrganizer>
+          <TeamMemberProfile
+            name="Lucas Onorato"
+            role="Owner/Founder"
+            bio="If you can dream it we can build it.” With five years of experience welding and fabricating, Lucas has turned his hobby into a business where anything can be made. With a background of working with talented local artists, Lucas quickly excelled in welding sculptures and decorative furnishings. With a passion for creativity and an eye for detail, Lucas can find the solutions clients need while fitting a unique artistic design."
+            picture="LucasProfile.png"
+          />
+          <TeamMemberProfile
+            name="Zachary Miner"
+            role="Shop Manager/CAD Engineer"
+            bio="Graduating from Colorado School of Mines with a degree in Mechanical Engineering, Zachary uses his engineering experience and forward-thinking design to create the 3D CAD design. He then uses his welding skills to bring these ideas to life. "
+            picture="ZachProfile.png"
+          />
+          <TeamMemberProfile
+            name="Hanna Rupp"
+            role="Business Manager"
+            bio="With a degree in Finance from the University of Denver and a background in Financial Planning and Analysis, Hanna oversees that the business runs smoothly. From bookkeeping to research, she keeps the numbers straight and the office in order."
+            picture="HannaProfile.png"
+          />
+        </TeamProfileOrganizer>
       </TeamSection>
     </AboutContainer>
   );
@@ -76,10 +83,22 @@ const SubHeader = styled.h2`
   color: white;
 `;
 
+const SubHeaderTwo = styled.h2`
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: 2rem;
+  color: black;
+`;
+
 const AboutText = styled.p`
   color: white;
   font-size: 1.2rem;
-  width: ;
+`;
+
+const TeamProfileOrganizer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  width: 100%;
+  gap: 3rem;
 `;
 
 const TeamSection = styled.section`
--- src/components/TeamMemberProfile.js
+++ src/components/TeamMemberProfile.js
@@ -16,14 +16,26 @@ export default TeamMemberProfile;
 
 const ProfileContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
 `;
 
-const ProfilePicture = styled.img``;
+const ProfilePicture = styled.img`
+  width: 100%;
+`;
 
-const ProfileName = styled.h3``;
+const ProfileName = styled.h3`
+  font-size: 2em;
+  margin: 10px 0px;
+`;
 
-const ProfileRole = styled.h4``;
+const ProfileRole = styled.h4`
+  font-size: 1.3rem;
+  color: lightgray;
+  margin: 0;
+`;
 
-const ProfileBio = styled.p``;
+const ProfileBio = styled.p`
+  font-size: 1.2rem;
+`;
