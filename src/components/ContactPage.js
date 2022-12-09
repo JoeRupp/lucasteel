bb27ea52dd368283c1b9c8f66e480f25da76e1c1
Joe Rupp <joerpp8@gmail.com>
2022-12-08 21:53:23 -0700

    Add contact page initial components and structure

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,12 +1,49 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "./Button";
 
 const ContactPage = () => {
   return (
-    <section>
-      <h1>Contact</h1>
-    </section>
+    <ContactContainer>
+      <ContactForm>
+        <Header>Contact us for a quote:</Header>
+        <Label>First Name</Label>
+        <Input type="text"></Input>
+        <Label>Last Name</Label>
+        <Input type="text"></Input>
+        <Label>Email</Label>
+        <Input type="email"></Input>
+        <Label>Phone</Label>
+        <Input type="text"></Input>
+        <Label>Message</Label>
+        <MessageArea type="text" rows="5" cols="70" />
+        <Button name="Submit" />
+      </ContactForm>
+      <ContactDetails>
+        <Header>Contact Info:</Header>
+        <Info>lucasteelandfabrication@gmail.com</Info>
+        <Info>(720) 329-9769</Info>
+      </ContactDetails>
+    </ContactContainer>
   );
 };
 
 export default ContactPage;
+
+const ContactContainer = styled.section`
+  width: 100vw;
+`;
+
+const ContactForm = styled.form``;
+
+const ContactDetails = styled.div``;
+
+const Header = styled.h1``;
+
+const Input = styled.input``;
+
+const Label = styled.label``;
+
+const MessageArea = styled.textarea``;
+
+const Info = styled.h2``;
