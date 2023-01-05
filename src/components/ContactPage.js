e269f98c4d4efcff6866a9c3646741ddf9802bdc
Joe Rupp <joerpp8@gmail.com>
2023-01-04 20:13:27 -0700

    Add contact form initial styling

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -5,25 +5,35 @@ import Button from "./Button";
 const ContactPage = () => {
   return (
     <ContactContainer>
-      <ContactForm>
-        <Header>Contact us for a quote:</Header>
-        <Label>First Name</Label>
-        <Input type="text"></Input>
-        <Label>Last Name</Label>
-        <Input type="text"></Input>
-        <Label>Email</Label>
-        <Input type="email"></Input>
-        <Label>Phone</Label>
-        <Input type="text"></Input>
-        <Label>Message</Label>
-        <MessageArea type="text" rows="5" cols="70" />
-        <Button name="Submit" />
-      </ContactForm>
       <ContactDetails>
         <Header>Contact Info:</Header>
         <Info>lucasteelandfabrication@gmail.com</Info>
         <Info>(720) 329-9769</Info>
       </ContactDetails>
+      <ContactForm>
+        <Instructions>Contact us for a quote:</Instructions>
+        <FormField>
+          <Label>First Name:</Label>
+          <Input type="text"></Input>
+        </FormField>
+        <FormField>
+          <Label>Last Name:</Label>
+          <Input type="text"></Input>
+        </FormField>
+        <FormField>
+          <Label>Email:</Label>
+          <Input type="email"></Input>
+        </FormField>
+        <FormField>
+          <Label>Phone:</Label>
+          <Input type="text"></Input>
+        </FormField>
+        <FormField>
+          <Label>Message:</Label>
+          <MessageArea type="text" rows="5" cols="70" />
+        </FormField>
+        <Button name="Submit" />
+      </ContactForm>
     </ContactContainer>
   );
 };
@@ -32,15 +42,29 @@ export default ContactPage;
 
 const ContactContainer = styled.section`
   width: 100vw;
+  margin-top: 10%;
+  width: 80%;
+  padding: 5% 10%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 `;
 
 const ContactForm = styled.form``;
 
+const FormField = styled.div``;
+
 const ContactDetails = styled.div``;
 
-const Header = styled.h1``;
+const Header = styled.h1`
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  font-size: 2rem;
+`;
 
-const Input = styled.input``;
+const Instructions = styled.h2``;
+
+const Input = styled.input`
+  height: 1.5rem;
+`;
 
 const Label = styled.label``;
 
