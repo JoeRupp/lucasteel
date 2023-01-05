1c01d2103419548cde5108d9c733b3e2a8b293f2
Joe Rupp <joerpp8@gmail.com>
2023-01-04 21:06:01 -0700

    Add contact form styling and correct spacing

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,34 +6,41 @@ const ContactPage = () => {
   return (
     <ContactContainer>
       <ContactDetails>
-        <Header>Contact Info:</Header>
+        <Header>Contact us for a quote:</Header>
         <Info>lucasteelandfabrication@gmail.com</Info>
         <Info>(720) 329-9769</Info>
       </ContactDetails>
       <ContactForm>
-        <Instructions>Contact us for a quote:</Instructions>
-        <FormField>
-          <Label>First Name:</Label>
-          <Input type="text"></Input>
-        </FormField>
-        <FormField>
-          <Label>Last Name:</Label>
-          <Input type="text"></Input>
-        </FormField>
-        <FormField>
-          <Label>Email:</Label>
-          <Input type="email"></Input>
-        </FormField>
-        <FormField>
-          <Label>Phone:</Label>
-          <Input type="text"></Input>
-        </FormField>
-        <FormField>
+        <FormOrganizer>
+          <FormField>
+            <Label>First Name:</Label>
+            <Input type="text"></Input>
+          </FormField>
+          <FormField>
+            <Label>Last Name:</Label>
+            <Input type="text"></Input>
+          </FormField>
+        </FormOrganizer>
+        <FormOrganizer>
+          <FormField>
+            <Label>Email:</Label>
+            <Input type="email"></Input>
+          </FormField>
+          <FormField>
+            <Label>Phone:</Label>
+            <Input type="text"></Input>
+          </FormField>
+        </FormOrganizer>
+        <FormFieldMessage>
           <Label>Message:</Label>
-          <MessageArea type="text" rows="5" cols="70" />
-        </FormField>
+          <MessageArea type="text" rows="5" cols="65" />
+        </FormFieldMessage>
         <Button name="Submit" />
       </ContactForm>
+      <FormCompletionMessage>
+        <Header>Form Submitted - Thank you!</Header>
+        <Info>We'll get back to you as soon as we can.</Info>
+      </FormCompletionMessage>
     </ContactContainer>
   );
 };
@@ -51,23 +58,43 @@ const ContactContainer = styled.section`
 
 const ContactForm = styled.form``;
 
-const FormField = styled.div``;
+const FormOrganizer = styled.div`
+  display: flex;
+`;
+
+const FormField = styled.div`
+  margin: 25px 5% 0 0;
+  width: 45%;
+`;
+
+const FormFieldMessage = styled.div`
+  margin: 25px 5% 25px 0;
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const ContactDetails = styled.div``;
+
+const Info = styled.h2``;
 
 const Header = styled.h1`
   font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
   font-size: 2rem;
+  margin-top: 25px;
 `;
-
-const Instructions = styled.h2``;
 
 const Input = styled.input`
   height: 1.5rem;
+  width: 100%;
 `;
 
-const Label = styled.label``;
+const Label = styled.label`
+  font-size: 1.2rem;
+`;
 
 const MessageArea = styled.textarea``;
 
-const Info = styled.h2``;
+const FormCompletionMessage = styled.div`
+  display: none;
+`;
