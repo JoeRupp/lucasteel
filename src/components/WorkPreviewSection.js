8b1d5e4374196a3e4e7ccb80cef20ce4367d3f96
Joe Rupp <joerpp8@gmail.com>
2023-01-06 10:55:34 -0700

    Add work preview styling

--- src/components/WorkPreviewSection.js
+++ src/components/WorkPreviewSection.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const WorkPreviewSection = ({ title, description, image }) => {
+const WorkPreviewSection = ({ title, description, image, link, id }) => {
   return (
-    <WorkContainer>
+    <WorkContainer to={`/project/${id}`}>
       <Image src={require(`../assets/photos/${image}`)} />
       <InfoContainer>
         <Title>{title}</Title>
@@ -31,25 +32,22 @@ const InfoContainer = styled.div`
 const Title = styled.h1`
   color: white;
   font-size: 2.5rem;
+  margin-bottom: 0;
   z-index: 1;
 `;
 
 const LineBreak = styled.div`
   width: 90%;
   background-color: white;
-  height: 1.2px;
-  margin: 0em 1em;
-  transition: all 0.35s;
+  height: 1px;
 `;
 
 const Description = styled.p`
   color: white;
   z-index: 1;
-  transition: all 0.35s;
-  display: none;
 `;
 
-const WorkContainer = styled.div`
+const WorkContainer = styled(Link)`
   width: 100vw;
   height: 90vh;
   position: relative;
@@ -58,16 +56,12 @@ const WorkContainer = styled.div`
   display: flex;
   justify-content: flex-end;
   flex-direction: column;
-  &:hover {
-    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
-  }
+  text-decoration: none;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8));
+  cursor: pointer;
   &:hover ${Image} {
     transition: all 0.35s;
     transform: scale(1.1);
     object-position: center;
-  }
-  &:hover ${Description} {
-    transition: all 0.35s;
-    display: contents;
   }
 `;
